d3a907e7b51751be98333b53336ea59bb5978568
Tuyen-0602 <tuyen0602.nguyen@gmail.com>
2022-06-21 23:21:02 +0700

    fix

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,6 @@
 var index = 1;
 changimg = function(){
-    var imgs = ["../img/3_1.jpg","../img/3_2.jpg", "../img/3_3.jpg", "../img/3_4.jpg", "../img/3_5.jpg"];
+    var imgs = ["./img/3_1.jpg","./img/3_2.jpg", "./img/3_3.jpg", "./img/3_4.jpg", "./img/3_5.jpg"];
     document.getElementById('img').src = imgs[index];
     index++;
     if(index==5){
