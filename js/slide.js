2ad1f57ec399f8f94440d1d3efc3cd3959c7db71
Tuyen-0602 <tuyen0602.nguyen@gmail.com>
2022-06-21 23:10:05 +0700

    upload

---
+++ js/slide.js
@@ -0,0 +1,10 @@
+var index = 1;
+changimg = function(){
+    var imgs = ["../img/3_1.jpg","../img/3_2.jpg", "../img/3_3.jpg", "../img/3_4.jpg", "../img/3_5.jpg"];
+    document.getElementById('img').src = imgs[index];
+    index++;
+    if(index==5){
+        index = 0;
+    }
+}
+setInterval(changimg,5000);
